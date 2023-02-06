07b9acf852e2e9edc509ed58a08a5ac316b123fe
Katherin Jimenez <katjimenez98@gmail.com>
2023-02-06 18:34:29 -0500

    Changed placement of potentialAnswer constant array from Answer model to AnswerController.

--- summative/src/main/java/com/company/summative/SummativeApplication.java
+++ summative/src/main/java/com/company/summative/SummativeApplication.java
@@ -1,11 +1,9 @@
 package com.company.summative;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SummativeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SummativeApplication.class, args);
 	}
--- summative/src/main/java/com/company/summative/controller/AnswerController.java
+++ summative/src/main/java/com/company/summative/controller/AnswerController.java
@@ -3,19 +3,35 @@ import com.company.summative.models.Answer;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class AnswerController {
+    private String[] potentialAnswers = {
+            "Signs point to yes",
+            "My reply is no.",
+            "Concentrate and ask again",
+            "It is certain",
+            "Outlook not so good",
+            "My sources say no"
+    };
+
     private List<Answer> answerList = new ArrayList<>();
     int idCounter = 0;
 
     @RequestMapping(value = "/magic", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public Answer addAnswer(@RequestBody String question) {
-        Answer a = new Answer(question, idCounter++);
+        if(question == "")
+            throw new IllegalArgumentException("You must provide a question in order to receive an answer.");
+
+        Answer a = new Answer();
+        a.setId(idCounter++);
+        a.setQuestion(question);
+        a.setAnswer(potentialAnswers[(int)(Math.random() * ((potentialAnswers.length-1) + 1))]);
         answerList.add(a);
         return a;
     }
