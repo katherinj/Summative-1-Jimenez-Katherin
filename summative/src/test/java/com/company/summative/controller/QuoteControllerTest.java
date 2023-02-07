c60fe94fdee263a7b6af7a06dd69722cba43c65e
Ashley Chompre <anc67@duke.edu>
2023-02-06 20:00:57 -0500

    added openapi documentation

---
+++ summative/src/test/java/com/company/summative/controller/QuoteControllerTest.java
@@ -0,0 +1,36 @@
+package com.company.summative.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(QuoteController.class)
+public class QuoteControllerTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        // Standard set up method, for instantiating test objects
+        // Don't have to do anything special for mockMvc since it's Autowired
+    }
+
+    @Test
+    public void shouldReturnAllQuotesInList() throws Exception {
+
+        // ACT
+        mockMvc.perform(get("/quote"))              // Perform the GET request
+                .andDo(print())                          // Print results to console
+                .andExpect(status().isOk());              // ASSERT (status code is 200)
+    }
+}
